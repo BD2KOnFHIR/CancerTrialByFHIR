b2f68c0c5be2ae5707080ee0469c47c68cb6097e
zongnansu1982 <zongnansu1982@users.noreply.github.com>
2020-09-18 09:18:51 -0500

    Create FHIRConcept_Bean.java

---
+++ code/model/FHIRConcept_Bean.java
@@ -0,0 +1,20 @@
+package ijmi.fhirModel;
+
+public class FHIRConcept_Bean {
+	String id;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	String text;
+	
+
+}
